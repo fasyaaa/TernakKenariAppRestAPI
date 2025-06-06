d88cfbbf7290fcfa128547ee029cc1ee152d270d
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-06-06 21:06:15 +0700

    ADD - LoginRequested event

--- lib/presentation/auth/bloc/login/login_event.dart
+++ lib/presentation/auth/bloc/login/login_event.dart
@@ -2,3 +2,9 @@ part of 'login_bloc.dart';
 
 @immutable
 sealed class LoginEvent {}
+
+class LoginRequested extends LoginEvent {
+  final LoginRequestModel requestModel;
+
+  LoginRequested({required this.requestModel});
+}
