103d24bd4c83e302d268004d9d6c3ac47d6e0fb3
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-06-06 22:00:02 +0700

    ADD - register event bloc

--- lib/presentation/auth/bloc/register/register_event.dart
+++ lib/presentation/auth/bloc/register/register_event.dart
@@ -2,3 +2,9 @@ part of 'register_bloc.dart';
 
 @immutable
 sealed class RegisterEvent {}
+
+class RegisterRequested extends RegisterEvent {
+  final RegisterRequestModel requestModel;
+
+  RegisterRequested({required this.requestModel});
+}
