a06263839f7b79bbd3c15e08718139df3310a73e
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-06-06 07:22:09 +0700

    ADD - Implement ProfileBuyerBloc with add and fetch logic

--- lib/presentation/buyer/profile/bloc/profile_bloc_bloc.dart
+++ lib/presentation/buyer/profile/bloc/profile_bloc_bloc.dart
@@ -1,13 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:carnary/data/model/request/buyer/buyer_profile_request_model.dart';
+import 'package:carnary/data/model/response/buyer/buyer_profile_response_model.dart';
+import 'package:carnary/data/repository/profile_buyer_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'profile_bloc_event.dart';
 part 'profile_bloc_state.dart';
 
-class ProfileBlocBloc extends Bloc<ProfileBlocEvent, ProfileBlocState> {
-  ProfileBlocBloc() : super(ProfileBlocInitial()) {
-    on<ProfileBlocEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
+  final ProfileBuyerRepository profileBuyerRepository;
+    ProfileBuyerBloc({required this.profileBuyerRepository}) : super(ProfileBuyerInitial()){on<AddProfileBuyerEvent>(_addProfileBuyer); on<GetProfileBuyerEvent>(_getProfileBuyer);}
+
+    Future<void> _addProfileBuyer(
+      AddProfileBuyerEvent event,
+      Emitter<ProfileBuyerState>emit
+    )  async {
+      emit(ProfileBuyerLoading());
+      final result = await profileBuyerRepository.addProfileBuyer(event.requestModel);
+      result.fold((error) => emit(ProfileBuyerAddError(message: error)), (profile) {
+        emit(ProfileBuyerAdded(profile: profile));
+      });
+    }
+
+    Future<void> _getProfileBuyer(GetProfileBuyerEvent event, Emitter<ProfileBuyerState> emit) async{
+      emit(ProfileBuyerLoading());
+      final result = await profileBuyerRepository.getProfileBuyer();
+      result.fold((error) => emit(ProfileBuyerError(message: error)), (profile) => emit(ProfileBuyerLoaded(profile: profile)));
+    }
 }
